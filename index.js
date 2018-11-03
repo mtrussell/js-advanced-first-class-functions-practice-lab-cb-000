8312798c7427845cb751cc67f60fb20683736151
Matt Trussell <smtrussell@gmail.com>
2018-11-03 18:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 // Code your solution in this file!
 
 
-function logDriverNames() {
-  
+function logDriverNames(drivers) {
+  drivers.forEach(function (curr){
+    console.log(curr.name);
+  });
 }
 
 
 
 function logDriversByHometown() {
-  
+
 }
 
 
 
 function driversByRevenue() {
-  
+
 }
 
 
 
 function driversByName() {
-  
+
 }
 
 
 
 function totalRevenue() {
-  
+
 }
 
 
 
 function averageRevenue() {
-  
+
 }
-
-
